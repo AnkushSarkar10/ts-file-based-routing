9cb8405ed039ef152be4c02551e170ce55d48510
Ankush Sarkar <ankush.sarkar2002@gmail.com>
2025-01-19 02:07:58 -0500

    something

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log("wip");
+import path from "path";
+import fg from "fast-glob";
+
+export interface Route {
+    path: string;
+    filePath: string;
+}
+
+export async function generateRoutes(directory: string): Promise<Route[]> {
+    const files = await fg("**/*.html", { cwd: directory });
+    return files.map((file: string) => {
+        const routePath = file
+            .replace(/index\.html$/, "") // Remove `index.html`
+            .replace(/\.html$/, "") // Remove `.html`
+            .replace(/\[([^\]]+)\]/g, ":$1"); // Convert `[id]` to `:id`
+
+        return {
+            path: `/${routePath}`.replace(/\/+/g, "/"), // Ensure single slash
+            filePath: path.join(directory, file),
+        };
+    });
+}
+
+generateRoutes("./routes").then((routes) => {
+    console.log(routes);
+});
